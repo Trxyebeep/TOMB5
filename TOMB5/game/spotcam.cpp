aa1835bf09cddd3f945a14d779b6325e1736f203
asasas9500 <asasas9500@gmail.com>
2021-04-10 14:43:46 -0300

    Improve Spline()

--- TOMB5/game/spotcam.cpp
+++ TOMB5/game/spotcam.cpp
@@ -4,32 +4,17 @@
 
 long Spline(long x, long* knots, int nk)
 {
-	int64_t v3 = x * (int64_t)(nk - 3) << 16 >> 16;
-	int32_t v4 = (int32_t)v3 >> 16;
-	if ((int32_t)v3 >> 16 >= nk - 3)
-		v4 = nk - 4;
-	int32_t v5 = knots[v4];
-	int32_t v6 = knots[v4 + 2];
-	int32_t nka = knots[v4 + 3] >> 1;
-	int32_t v7 = knots[v4 + 1];
+	int span;
+	long *k, c1, c2;
 
-	return (int32_t)(v7
-		+ (int64_t)(uint64_t)((int32_t)((~v5 >> 1)
-			+ (v6 >> 1)
-			+ (int64_t)(uint64_t)((int32_t)(v5
-				+ (int64_t)(uint64_t)(((~v5 >> 1)
-					+ nka
-					+ v7
-					+ (v7 >> 1)
-					- (v6 >> 1)
-					- v6)
-					* (int64_t)((int32_t)v3 - (v4 << 16)) >> 16)
-				- 2 * v7
-				+ 2 * v6
-				- (v7 >> 1)
-				- nka)
-				* (int64_t)((int32_t)v3 - (v4 << 16)) >> 16))
-			* (int64_t)((int32_t)v3 - (v4 << 16)) >> 16));
+	span = x * (nk - 3) >> 16;
+	if (span >= nk - 3)
+		span = nk - 4;
+	k = &knots[span];
+	x = x * (nk - 3) - span * 65536;
+	c1 = (k[1] >> 1) - (k[2] >> 1) - k[2] + k[1] + (k[3] >> 1) + ((-k[0] - 1) >> 1);
+	c2 = 2 * k[2] - 2 * k[1] - (k[1] >> 1) - (k[3] >> 1) + k[0];
+	return ((long long) x * (((long long) x * (((long long) x * c1 >> 16) + c2) >> 16) + (k[2] >> 1) + ((-k[0] - 1) >> 1)) >> 16) + k[1];
 }
 
 void inject_spotcam()
