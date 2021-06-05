193a668a480a3e2e72d41b494a4e0612bdd3b0ee
Troye <woopstombraider@gmail.com>
2021-06-05 11:31:54 -0500

    reform DoGameflow();

--- TOMB5/game/gameflow.cpp
+++ TOMB5/game/gameflow.cpp
@@ -72,13 +72,12 @@ void DoGameflow()
 {
 	unsigned char* gf;
 	unsigned char n;
-	unsigned char* m;
 
 	do_boot_screen(Gameflow->Language);
 	num_fmvs = 0;
 	fmv_to_play[1] = 0;
 	fmv_to_play[0] = 0;
-	gfCurrentLevel = Gameflow->TitleEnabled == 0;
+	gfCurrentLevel = Gameflow->TitleEnabled ? 0 : 1;
 	gf = &gfScriptWad[gfScriptOffset[gfCurrentLevel]];
 
 	while (1)
@@ -87,7 +86,7 @@ void DoGameflow()
 		{
 		case GF_FMV:
 			fmv_to_play[num_fmvs++] = *gf++;
-			continue;
+			break;
 
 		case GF_LEVEL:
 			gfLevelFlags = gf[1] | (gf[2] << 8);
@@ -113,38 +112,38 @@ void DoGameflow()
 			gfFog.b = 0;
 			gfFog.a = 0;
 
-			if (gfStatus != 2)
+			switch (gfStatus)
 			{
-				if (gfStatus < 3)
-				{
-					if (gfStatus == 1)
-					{
-						gfInitialiseGame = 1;
-						gfCurrentLevel = Gameflow->TitleEnabled == 0;
-					}
-				}
-				else
-				{
-					if (gfStatus == 3)
-					{
-						if (!Gameflow->DemoDisc && Gameflow->nLevels != 2 && gfLevelComplete <= Gameflow->nLevels)
-							gfCurrentLevel = gfLevelComplete;
-						else
-							gfCurrentLevel = 0;
-					}
-					else if (gfStatus == 4)
-						return;
-				}
-			}
-			else
-			{
+			case 1:
+				gfInitialiseGame = 1;
+				gfCurrentLevel = Gameflow->TitleEnabled == 0;
+				break;
+
+			case 2:
 				gfGameMode = 4;
 				gfCurrentLevel = savegame.CurrentLevel;
+				break;
+
+			case 3:
+				if (Gameflow->DemoDisc || Gameflow->nLevels == 2)
+					gfCurrentLevel = 0;
+				else
+				{
+					if (gfLevelComplete > Gameflow->nLevels)
+						gfCurrentLevel = 0;
+					else
+						gfCurrentLevel = gfLevelComplete;
+				}
+
+				break;
+
+			case 4:
+				return;
 			}
 
 			gf = &gfScriptWad[gfScriptOffset[gfCurrentLevel]];
-
 			break;
+
 		case GF_TITLE_LEVEL:
 			gfLevelFlags = gf[0] | (gf[1] << 8);
 			DoTitle(gf[2], gf[3]);
@@ -154,48 +153,49 @@ void DoGameflow()
 			gfNumMips = 0;
 			gfNumPickups = 0;
 
-			if (gfStatus == 3)
+			switch (gfStatus)
 			{
+			case 2:
+				gfGameMode = 4;
+				gfCurrentLevel = savegame.CurrentLevel;
+				break;
+
+			case 3:
 				gfGameMode = 0;
 				gfCurrentLevel = gfLevelComplete;
 				gfInitialiseGame = 1;
-			}
-			else if (gfStatus < 4)
-			{
-				if (gfStatus == 2)
-				{
-					gfGameMode = 4;
-					gfCurrentLevel = savegame.CurrentLevel;
-				}
-			}
-			else if (gfStatus == 4)
+				break;
+
+			case 4:
 				return;
+			}
 
 			gf = &gfScriptWad[gfScriptOffset[gfCurrentLevel]];
 
 			break;
+
 		case GF_LEVEL_DATA_END:
-			continue;
+			break;
 
 		case GF_CUT:
 			gfCutNumber = *gf++;
-			continue;
+			break;
 
 		case GF_RESIDENTCUT1:
 			gfResidentCut[0] = *gf++;
-			continue;
+			break;
 
 		case GF_RESIDENTCUT2:
 			gfResidentCut[1] = *gf++;
-			continue;
+			break;
 
 		case GF_RESIDENTCUT3:
 			gfResidentCut[2] = *gf++;
-			continue;
+			break;
 
 		case GF_RESIDENTCUT4:
 			gfResidentCut[3] = *gf++;
-			continue;
+			break;
 
 		case GF_LAYER1:
 			LightningRGB[0] = *gf;
@@ -208,7 +208,7 @@ void DoGameflow()
 			LightningRGBs[2] = *gf;
 			gfLayer1Col.b = *gf++;
 			gfLayer1Vel = *gf++;
-			continue;
+			break;
 
 		case GF_LAYER2:
 			LightningRGB[0] = *gf;
@@ -221,11 +221,11 @@ void DoGameflow()
 			LightningRGBs[2] = *gf;
 			gfLayer2Col.b = *gf++;
 			gfLayer2Vel = *gf++;
-			continue;
+			break;
 
 		case GF_UV_ROTATE:
 			gfUVRotate = *gf++;
-			continue;
+			break;
 
 		case GF_LEGEND:
 			gfLegend = *gf++;
@@ -233,49 +233,47 @@ void DoGameflow()
 			if (gfGameMode != 4)
 				gfLegendTime = 150;
 
-			continue;
+			break;
 
 		case GF_LENS_FLARE:
 			gfLensFlare.x = ((gf[1] << 8) | gf[0]) << 8;
-			gfLensFlare.y = ((gf[2] | (gf[3] << 8)) << 16) >> 8;
+			gfLensFlare.y = ((gf[3] << 8) | gf[2]) << 8;
 			gfLensFlare.z = ((gf[5] << 8) | gf[4]) << 8;
 			gfLensFlareColour.r = gf[6];
 			gfLensFlareColour.g = gf[7];
 			gfLensFlareColour.b = gf[8];
 			gf += 9;
-			continue;
+			break;
 
 		case GF_MIRROR:
 			gfMirrorRoom = *gf;
 			gfMirrorZPlane = (gf[4] << 24) | (gf[3] << 16) | (gf[2] << 8) | gf[1];
 			gf += 5;
-			continue;
+			break;
 
 		case GF_FOG:
-			m = gf++;
-			gfFog.b = *gf;
-			gfFog.g = *(char*)(m)++;
-			gfFog.r = *(char*)(gf++);
-			gf = (unsigned __int8*)((char*)(gf++) + 1);
-			continue;
+			gfFog.r = *gf++;
+			gfFog.g = *gf++;
+			gfFog.b = *gf++;
+			break;
 
 		case GF_ANIMATING_MIP:
 			gfMips[gfNumMips++] = *gf++;
-			continue;
+			break;
 
 		case GF_RESET_HUB:
 			gfResetHubDest = *gf++;
-			continue;
+			break;
 
 		case GF_GIVE_ITEM_AT_STARTUP:
 			gfPickups[gfNumPickups++] = *gf;
 			gf += 2;
-			continue;
+			break;
 
 		case GF_LOSE_ITEM_AT_STARTUP:
 			gfTakeaways[gfNumTakeaways++] = *gf;
 			gf += 2;
-			continue;
+			break;
 
 		default:
 
@@ -302,7 +300,7 @@ void DoGameflow()
 			inventry_objects_list[n].zrot = gf[10] | (gf[11] << 8);
 			inventry_objects_list[n].flags = gf[12] | (gf[13] << 8);
 			gf += 14;
-			continue;
+			break;
 		}
 	}
 }
