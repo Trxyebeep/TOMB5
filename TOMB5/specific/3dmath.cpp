67b13c54cf246aa582a66812ab7f359e0e74fabb
Troye <woopstombraider@gmail.com>
2021-09-30 10:57:44 -0500

    implement TR2Main widescreen;

--- TOMB5/specific/3dmath.cpp
+++ TOMB5/specific/3dmath.cpp
@@ -13,11 +13,25 @@ void AlterFOV(short fov)
 
 	CurrentFov = fov;
 	fov /= 2;
+
+#ifdef TR2MAIN_WIDESCREEN	//by Arsunt
+	long fov_width;
+
+	fov_width = phd_winheight * 320 / 240;
+	LfAspectCorrection = 1.0F; // must always be 1.0 for unstretched view
+	phd_persp = (fov_width / 2) * phd_cos(fov) / phd_sin(fov);
+#else
 	phd_persp = ((phd_winwidth / 2) * phd_cos(fov)) / phd_sin(fov);
+#endif
+
 	f_persp = float(phd_persp);
 	f_oneopersp = one / f_persp;
 	f_perspoznear = f_persp / f_znear;
-	LfAspectCorrection = (4.0f / 3.0f) / (float(phd_winwidth) / float(phd_winheight));
+
+#ifndef TR2MAIN_WIDESCREEN
+	LfAspectCorrection = (4.0F / 3.0F) / (float(phd_winwidth) / float(phd_winheight));
+#endif
+
 	f_mpersp = f_persp;
 	f_moneopersp = mone / f_persp;
 	f_mperspoznear = f_persp / f_mznear;
