85baf7d673312fe300cbba269255dc06ac41c9d3
Troye <woopstombraider@gmail.com>
2021-11-17 12:01:50 -0600

    reg read values

--- TOMB5/specific/registry.cpp
+++ TOMB5/specific/registry.cpp
@@ -61,7 +61,7 @@ void REG_WriteString(char* SubKeyName, char* string, long length)
 		else
 			checkLength = length;
 
-		RegSetValueEx(phkResult, SubKeyName, 0, REG_DWORD, (CONST BYTE*)string, checkLength + 1);
+		RegSetValueEx(phkResult, SubKeyName, 0, REG_SZ, (CONST BYTE*)string, checkLength + 1);
 	}
 	else
 		RegDeleteValue(phkResult, SubKeyName);
@@ -84,6 +84,103 @@ void REG_WriteVoid(char* SubKeyName, void* value, long size)
 		RegDeleteValue(phkResult, SubKeyName);
 }
 
+bool REG_ReadLong(char* SubKeyName, ulong& value, ulong defaultValue)
+{
+	ulong type;
+	ulong cbData;
+
+	cbData = 4;
+
+	if (RegQueryValueEx(phkResult, SubKeyName, 0, &type, (LPBYTE)&value, &cbData) == ERROR_SUCCESS && type == REG_DWORD && cbData == 4)
+		return 1;
+
+	REG_WriteLong(SubKeyName, defaultValue);	//value not found, write default
+	value = defaultValue;
+	return 0;
+}
+
+bool REG_ReadBool(char* SubKeyName, bool& value, bool defaultValue)
+{
+	ulong type;
+	ulong cbData;
+	ulong data;
+
+	cbData = 4;
+
+	if (RegQueryValueEx(phkResult, SubKeyName, 0, &type, (LPBYTE)&data, &cbData) == ERROR_SUCCESS && type == REG_DWORD && cbData == 4)
+	{
+		value = (bool)data;
+		return 1;
+	}
+
+	REG_WriteBool(SubKeyName, defaultValue);
+	value = defaultValue;
+	return 0;
+}
+
+bool REG_ReadString(char* SubKeyName, char* value, long length, char* defaultValue)
+{
+	ulong type;
+	ulong cbData;
+	long len;
+
+	cbData = length;
+
+	if (RegQueryValueEx(phkResult, SubKeyName, 0, &type, (LPBYTE)value, (LPDWORD)&cbData) == ERROR_SUCCESS && type == REG_SZ)
+		return 1;
+
+	if (defaultValue)
+	{
+		REG_WriteBool(SubKeyName, 1);	//this feels wrong.. probably because of the original overloaded names
+		len = strlen(defaultValue) + 1;
+
+		if (len > length)
+		{
+			len = length - 1;
+			value[len] = 0;
+		}
+
+		memcpy(value, defaultValue, len);
+	}
+	else
+		RegDeleteValue(phkResult, SubKeyName);
+
+	return 0;
+}
+
+bool REG_ReadDouble(char* SubKeyName, double& value, double defaultValue)
+{
+	char buf[64];
+
+	if (REG_ReadString(SubKeyName, buf, sizeof(buf), 0))
+	{
+		value = atof(buf);
+		return 1;
+	}
+
+	REG_WriteDouble(SubKeyName, defaultValue);
+	value = defaultValue;
+	return 0;
+}
+
+bool REG_ReadVoid(char* SubKeyName, void* value, long size, void* defaultValue)
+{
+	ulong type;
+	ulong cbData;
+
+	cbData = size;
+
+	if (RegQueryValueEx(phkResult, SubKeyName, 0, &type, (LPBYTE)value, &cbData) == ERROR_SUCCESS && type == REG_BINARY && size == cbData)
+		return 1;
+
+	if (defaultValue)
+		REG_WriteVoid(SubKeyName, value, size);
+	else
+		RegDeleteValue(phkResult, SubKeyName);
+
+	return 0;
+}
+
 void inject_registry(bool replace)
 {
 	INJECT(0x004BD860, REG_OpenKey, replace);
@@ -97,4 +194,9 @@ void inject_registry(bool replace)
 	INJECT(0x004BD9E0, REG_WriteString, replace);
 	INJECT(0x004BD950, REG_WriteDouble, replace);
 	INJECT(0x004BD990, REG_WriteVoid, replace);
+	INJECT(0x004BDA80, REG_ReadLong, replace);
+	INJECT(0x004BDB00, REG_ReadBool, replace);
+	INJECT(0x004BDCC0, REG_ReadString, replace);
+	INJECT(0x004BDBA0, REG_ReadDouble, replace);
+	INJECT(0x004BDC20, REG_ReadVoid, replace);
 }
--- TOMB5/specific/registry.h
+++ TOMB5/specific/registry.h
@@ -14,3 +14,8 @@ void REG_WriteBool(char* SubKeyName, bool value);
 void REG_WriteString(char* SubKeyName, char* string, long length);
 void REG_WriteDouble(char* SubKeyName, double value);
 void REG_WriteVoid(char* SubKeyName, void* value, long size);
+bool REG_ReadLong(char* SubKeyName, ulong& value, ulong defaultValue);
+bool REG_ReadBool(char* SubKeyname, bool& value, bool defaultValue);
+bool REG_ReadString(char* SubKeyName, char* value, long length, char* defaultValue);
+bool REG_ReadDouble(char* SubKeyName, double& value, double defaultValue);
+bool REG_ReadVoid(char* SubKeyName, void* value, long size, void* defaultValue);
