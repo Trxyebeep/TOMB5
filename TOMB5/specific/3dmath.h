b92129f06b8e621b6a9bcf7f2d2e4112a07a9e0e
Troye <woopstombraider@gmail.com>
2022-01-24 01:43:40 -0600

    phd_LookAt(); aLookAt();

--- TOMB5/specific/3dmath.h
+++ TOMB5/specific/3dmath.h
@@ -29,11 +29,12 @@ void phd_RotYXZ(short y, short x, short z);
 void phd_TranslateAbs(long x, long y, long z);
 void phd_GetVectorAngles(long x, long y, long z, short* angles);
 void phd_TransposeMatrix();
+void phd_LookAt(long xsrc, long ysrc, long zsec, long xtar, long ytar, long ztar, short roll);
+void aLookAt(float xsrc, float ysrc, float zsrc, float xtar, float ytar, float ztar, long roll);
 
 #define ScaleCurrentMatrix	( (void(__cdecl*)(VECTOR*)) 0x0048EFF0 )
 #define mGetAngle	( (long(__cdecl*)(long, long, long, long)) 0x0048F290 )
 #define phd_GenerateW2V	( (void(__cdecl*)(PHD_3DPOS*)) 0x0048F330 )
-#define phd_LookAt	( (void(__cdecl*)(long, long, long, long, long, long, short)) 0x0048F760 )
 #define phd_atan	( (long(__cdecl*)(long, long)) 0x0048F8A0 )
 #define phd_sqrt	( (long(__cdecl*)(long)) 0x0048F980 )
-#define aLookAt	( (void(__cdecl*)(float, float, float, float, float, float, int)) 0x00490C20 )
+#define aPointCamera	( (void(__cdecl*)(FCAMERA*)) 0x00490F30 )
