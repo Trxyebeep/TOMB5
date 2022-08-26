e66cd7a83c950b87eff84aa936bb534f694b3cc9
Troye <woopstombraider@gmail.com>
2022-08-26 03:10:52 -0500

    aGenerateMipMaps

--- TOMB5/specific/texture.cpp
+++ TOMB5/specific/texture.cpp
@@ -1,5 +1,7 @@
 #include "../tomb5/pch.h"
 #include "texture.h"
+#include "function_stubs.h"
+#include "dxshell.h"
 
 void AdjustTextInfo(PHDTEXTURESTRUCT* ptex, long num, TEXTURESTRUCT* tex)
 {
@@ -62,10 +64,74 @@ void RGBMLightMap(uchar* r, uchar* g, uchar* b)
 	*b = (255 - *b) >> 1;
 }
 
+void aGenerateMipMaps(LPDDSURFACEDESC2 pDesc, LPDIRECTDRAWSURFACE4 pDest, long* pSrc, long count)
+{
+	LPDIRECTDRAWSURFACE4 dest;
+	DDSCAPS2 caps;
+	DDSURFACEDESC2 desc;
+	char* pSurf;
+	char* pcSrc;
+	char* pcSrcT;
+	long x, y, num;
+	uchar r, g, b, a;
+
+	Log(5, "Create MipMap");
+	dest = pDest;
+	x = pDesc->dwWidth >> 1;
+	y = pDesc->dwHeight >> 1;
+	num = 8;
+	caps.dwCaps = DDSCAPS_TEXTURE | DDSCAPS_MIPMAP;
+
+	for (int i = 0; i < count; i++)
+	{
+		memset(&desc, 0, sizeof(DDSURFACEDESC2));
+		desc.dwSize = sizeof(DDSURFACEDESC2);
+		DXAttempt(dest->GetAttachedSurface(&caps, &dest));
+		DXAttempt(dest->Lock(0, &desc, DDLOCK_NOSYSLOCK, 0));
+		pSurf = (char*)desc.lpSurface;
+		pcSrc = (char*)pSrc;
+		Log(5, "Level %d - %8.8x", i, pSurf);
+
+		for (int j = 0; j < y; j++)
+		{
+			pcSrcT = pcSrc;
+
+			for (int k = 0; k < x; k++)
+			{
+				//bgra!!!
+				b = *pcSrcT++;
+				g = *pcSrcT++;
+				r = *pcSrcT++;
+				a = *pcSrcT++;
+
+				if (!r && !g && !b)
+				{
+					r = 255;
+					g = 255;
+				}
+
+				*pSurf++ = b;
+				*pSurf++ = g;
+				*pSurf++ = r;
+				*pSurf++ = a;
+				pcSrcT += num;
+			}
+
+			pcSrc += num << 8;
+		}
+
+		DXAttempt(dest->Unlock(0));
+		x >>= 1;
+		y >>= 1;
+		num >>= 1;
+	}
+}
+
 void inject_texture(bool replace)
 {
 	INJECT(0x004D01D0, AdjustTextInfo, replace);
 	INJECT(0x004D03A0, FindHighestBit, replace);
 	INJECT(0x004D03D0, RGBMDarkMap, replace);
 	INJECT(0x004D0400, RGBMLightMap, replace);
+	INJECT(0x004D0980, aGenerateMipMaps, replace);
 }
--- TOMB5/specific/texture.h
+++ TOMB5/specific/texture.h
@@ -7,6 +7,7 @@ void AdjustTextInfo(PHDTEXTURESTRUCT* ptex, long num, TEXTURESTRUCT* tex);
 long FindHighestBit(long n);
 void RGBMDarkMap(uchar* r, uchar* g, uchar* b);
 void RGBMLightMap(uchar* r, uchar* g, uchar* b);
+void aGenerateMipMaps(LPDDSURFACEDESC2 pDesc, LPDIRECTDRAWSURFACE4 pDest, long* pSrc, long count);
 
 #define ShowTextures	( (void(__cdecl*)()) 0x004D0CC0 )
 typedef void(__cdecl* rgbfunc)(uchar*, uchar*, uchar*);
