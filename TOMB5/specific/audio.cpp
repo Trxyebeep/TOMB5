44b5dff8404a961082dbfa0100a2dd48a66894de
Troye <woopstombraider@gmail.com>
2022-08-26 08:38:16 -0500

    ACMSetVolume

--- TOMB5/specific/audio.cpp
+++ TOMB5/specific/audio.cpp
@@ -1,7 +1,7 @@
 #include "../tomb5/pch.h"
 #include "audio.h"
 
-void S_CDPlay(short track, long mode)
+void S_CDPlay(long track, long mode)
 {
     if (acm_ready)
     {
@@ -28,7 +28,7 @@ void S_CDStop()
     }
 }
 
-void S_CDFade()
+void S_CDFade(long n)
 {
 
 }
@@ -39,10 +39,24 @@ void S_StartSyncedAudio(long track)
     S_CDPlay(track, 2);
 }
 
+void ACMSetVolume()
+{
+    long volume;
+
+    if (!MusicVolume)
+        volume = -10000;
+    else
+        volume = -4000 * (100 - MusicVolume) / 100;
+
+    if (DSBuffer)
+        DSBuffer->SetVolume(volume);
+}
+
 void inject_audio(bool replace)
 {
     INJECT(0x00492990, S_CDPlay, replace);
     INJECT(0x004929E0, S_CDStop, replace);
     INJECT(0x00492AA0, S_CDFade, replace);
     INJECT(0x00492AC0, S_StartSyncedAudio, replace);
+    INJECT(0x00492AF0, ACMSetVolume, replace);
 }
--- TOMB5/specific/audio.h
+++ TOMB5/specific/audio.h
@@ -3,11 +3,11 @@
 
 void inject_audio(bool replace);
 
-void S_CDPlay(short track, long mode);
+void S_CDPlay(long track, long mode);
 void S_CDStop();
-void S_CDFade();
+void S_CDFade(long n);
 void S_StartSyncedAudio(long track);
+void ACMSetVolume();
 
-#define ACMSetVolume	( (void(__cdecl*)()) 0x00492AF0 )
-#define ACMEmulateCDPlay    ( (void(__cdecl*)(short, long)) 0x00493760 )
+#define ACMEmulateCDPlay    ( (void(__cdecl*)(long, long)) 0x00493760 )
 #define ACMClose	( (void(__cdecl*)()) 0x004931A0 )
