d24795a91bc0ac3cb404907fa10ccf47d8734793
Troye <woopstombraider@gmail.com>
2022-08-27 02:38:57 -0500

    DXSetCooperativeLevel

--- TOMB5/specific/dxshell.cpp
+++ TOMB5/specific/dxshell.cpp
@@ -2,6 +2,36 @@
 #include "dxshell.h"
 #include "function_stubs.h"
 
+long DDSCL_FLAGS[11] =	// for DXSetCooperativeLevel logging
+{
+	DDSCL_ALLOWMODEX,
+	DDSCL_ALLOWREBOOT,
+	DDSCL_CREATEDEVICEWINDOW,
+	DDSCL_EXCLUSIVE,
+	DDSCL_FPUSETUP,
+	DDSCL_FULLSCREEN,
+	DDSCL_MULTITHREADED,
+	DDSCL_NORMAL,
+	DDSCL_NOWINDOWCHANGES,
+	DDSCL_SETDEVICEWINDOW,
+	DDSCL_SETFOCUSWINDOW
+};
+
+const char* DDSCL_TEXT[11] =
+{
+	"allowmodex",
+	"allowreboot",
+	"createdevicewindow",
+	"exclusive",
+	"fpusetup",
+	"fullscreen",
+	"multithreaded",
+	"normal",
+	"nowindowchanges",
+	"setdevicewindow",
+	"setfocuswindow"
+};
+
 void DXReadKeyboard(char* KeyMap)
 {
 #ifndef GENERAL_FIXES	//Arsunt's fix for keyboard deadlock, reproduceable in TR5 when a debugger breakpoint is hit..
@@ -114,6 +144,34 @@ long DXD3DCreate(LPDIRECTDRAW4 pDD4, void** pD3D)
 	return 1;
 }
 
+long DXSetCooperativeLevel(LPDIRECTDRAW4 pDD4, HWND hwnd, long flags)
+{
+	char* ptr;
+	char buf[1024];
+
+	strcpy(buf, "DXSetCooperativeLevel - ");
+	ptr = &buf[strlen(buf)];
+
+	for (int i = 0; i < 11; i++)
+	{
+		if (DDSCL_FLAGS[i] & flags)
+		{
+			strcpy(ptr, DDSCL_TEXT[i]);
+			ptr += strlen(DDSCL_TEXT[i]);
+			*ptr++ = ' ';
+			*ptr = '\0';
+		}
+	}
+
+	*(ptr - 1) = '\0';
+	Log(2, buf);
+
+	if (DXAttempt(pDD4->SetCooperativeLevel(hwnd, flags)) != DD_OK)
+		return 0;
+
+	return 1;
+}
+
 void inject_dxshell(bool replace)
 {
 	INJECT(0x004A2880, DXReadKeyboard, replace);
@@ -123,4 +181,5 @@ void inject_dxshell(bool replace)
 	INJECT(0x0049F4C0, AddStruct, replace);
 	INJECT(0x0049F530, DXDDCreate, replace);
 	INJECT(0x0049F620, DXD3DCreate, replace);
+	INJECT(0x004A0600, DXSetCooperativeLevel, replace);
 }
--- TOMB5/specific/dxshell.h
+++ TOMB5/specific/dxshell.h
@@ -10,6 +10,7 @@ long DIAttempt(HRESULT r);
 void* AddStruct(void* p, long num, long size);
 long DXDDCreate(LPGUID pGuid, void** pDD4);
 long DXD3DCreate(LPDIRECTDRAW4 pDD4, void** pD3D);
+long DXSetCooperativeLevel(LPDIRECTDRAW4 pDD4, HWND hwnd, long flags);
 
 #define DXUpdateJoystick	( (void(__cdecl*)()) 0x004A2D00 )
 #define DXCreateSurface	( (long(__cdecl*)(LPDIRECTDRAW4, LPDDSURFACEDESC2, LPDIRECTDRAWSURFACE4*)) 0x004A0520 )
