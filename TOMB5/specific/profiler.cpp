8b89926a059e91b0bbf9c0f4198800af32bcd371
Troye <woopstombraider@gmail.com>
2022-12-03 16:35:10 -0600

    mResetTime

--- TOMB5/specific/profiler.cpp
+++ TOMB5/specific/profiler.cpp
@@ -1,6 +1,8 @@
 #include "../tomb5/pch.h"
 #include "profiler.h"
 
+static __int64 counter;
+
 void mDrawTriangle(long x1, long y1, long x2, long y2, long x3, long y3, long c0, long c1, long c2)
 {
 
@@ -11,8 +13,14 @@ void mAddProfilerEvent(long c)
 
 }
 
+void mResetTime()
+{
+	QueryPerformanceCounter((LARGE_INTEGER*)&counter);
+}
+
 void inject_profiler(bool replace)
 {
 	INJECT(0x004BD3A0, mDrawTriangle, replace);
 	INJECT(0x004BD590, mAddProfilerEvent, replace);
+	INJECT(0x004BD3C0, mResetTime, replace);
 }
--- TOMB5/specific/profiler.h
+++ TOMB5/specific/profiler.h
@@ -5,5 +5,6 @@ void inject_profiler(bool replace);
 
 void mDrawTriangle(long x1, long y1, long x2, long y2, long x3, long y3, long c0, long c1, long c2);
 void mAddProfilerEvent(long c);
+void mResetTime();
 
 #define mDrawProfiler	( (void(__cdecl*)(double, long, long)) 0x004BD5B0 )
