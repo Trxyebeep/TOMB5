c6be7451c6bf165f79d8bbaaff0addc73405bfdd
Troye <woopstombraider@gmail.com>
2022-12-03 02:39:28 -0600

    SetD3DViewMatrix

--- TOMB5/specific/d3dmatrix.cpp
+++ TOMB5/specific/d3dmatrix.cpp
@@ -1,5 +1,6 @@
 #include "../tomb5/pch.h"
 #include "d3dmatrix.h"
+#include "dxshell.h"
 
 void SetD3DMatrixF(LPD3DMATRIX dest, float* src)
 {
@@ -56,9 +57,28 @@ void SaveD3DCameraMatrix()
 	D3DCameraMatrix._43 = aMXPtr[M23];
 }
 
+void SetD3DViewMatrix()
+{
+	D3DIdentityMatrix(&D3DMView);
+	D3DMView._11 = (float)phd_mxptr[M00] / 16384;
+	D3DMView._12 = (float)phd_mxptr[M10] / 16384;
+	D3DMView._13 = (float)phd_mxptr[M20] / 16384;
+	D3DMView._21 = (float)phd_mxptr[M01] / 16384;
+	D3DMView._22 = (float)phd_mxptr[M11] / 16384;
+	D3DMView._23 = (float)phd_mxptr[M21] / 16384;
+	D3DMView._31 = (float)phd_mxptr[M02] / 16384;
+	D3DMView._32 = (float)phd_mxptr[M12] / 16384;
+	D3DMView._33 = (float)phd_mxptr[M22] / 16384;
+	D3DMView._41 = float(phd_mxptr[M03] >> 14);
+	D3DMView._42 = float(phd_mxptr[M13] >> 14);
+	D3DMView._43 = float(phd_mxptr[M23] >> 14);
+	DXAttempt(App.dx.lpD3DDevice->SetTransform(D3DTRANSFORMSTATE_VIEW, &D3DMView));
+}
+
 void inject_d3dmatrix(bool replace)
 {
 	INJECT(0x00497550, SetD3DMatrixF, replace);
 	INJECT(0x00497910, D3DIdentityMatrix, replace);
 	INJECT(0x00497280, SaveD3DCameraMatrix, replace);
+	INJECT(0x00497320, SetD3DViewMatrix, replace);
 }
