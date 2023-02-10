afb8ad52c93ea80dcf784c8344fc42612af07dd7
Troye <woopstombraider@gmail.com>
2023-02-09 23:58:40 -0600

    LaraTestClimbPos

--- TOMB5/game/laraclmb.cpp
+++ TOMB5/game/laraclmb.cpp
@@ -610,6 +610,44 @@ long LaraTestClimb(long x, long y, long z, long xfront, long zfront, long item_h
 	return 1;
 }
 
+long LaraTestClimbPos(ITEM_INFO* item, long front, long right, long origin, long height, long* shift)
+{
+	long angle, x, z, xfront, zfront;
+
+	xfront = 0;
+	zfront = 0;
+	angle = ushort(item->pos.y_rot + 0x2000) / 0x4000;
+
+	switch (angle)
+	{
+	case NORTH:
+		x = right + item->pos.x_pos;
+		z = front + item->pos.z_pos;
+		zfront = 256;
+		break;
+
+	case EAST:
+		x = front + item->pos.x_pos;
+		z = item->pos.z_pos - right;
+		xfront = 256;
+		break;
+
+	case SOUTH:
+		x = item->pos.x_pos - right;
+		z = item->pos.z_pos - front;
+		zfront = -256;
+		break;
+
+	default:
+		x = item->pos.x_pos - front;
+		z = right + item->pos.z_pos;
+		xfront = -256;
+		break;
+	}
+
+	return LaraTestClimb(x, origin + item->pos.y_pos, z, xfront, zfront, height, item->room_number, shift);
+}
+
 void inject_laraclmb(bool replace)
 {
 	INJECT(0x00450D40, lara_as_climbstnc, replace);
@@ -628,4 +666,5 @@ void inject_laraclmb(bool replace)
 	INJECT(0x004523A0, GetClimbTrigger, replace);
 	INJECT(0x00451200, LaraTestClimbUpPos, replace);
 	INJECT(0x00450A50, LaraTestClimb, replace);
+	INJECT(0x00450970, LaraTestClimbPos, replace);
 }
--- TOMB5/game/laraclmb.h
+++ TOMB5/game/laraclmb.h
@@ -18,6 +18,6 @@ void lara_col_climbdown(ITEM_INFO* item, COLL_INFO* coll);
 short GetClimbTrigger(long x, long y, long z, short room_number);
 long LaraTestClimbUpPos(ITEM_INFO* item, long front, long right, long* shift, long* ledge);
 long LaraTestClimb(long x, long y, long z, long xfront, long zfront, long item_height, short item_room, long* shift);
+long LaraTestClimbPos(ITEM_INFO* item, long front, long right, long origin, long height, long* shift);
 
 #define	LaraDoClimbLeftRight	( (void(__cdecl*)(ITEM_INFO*, COLL_INFO*, long, long)) 0x004515A0 )
-#define LaraTestClimbPos	( (long(__cdecl*)(ITEM_INFO*, long, long, long, long, long*)) 0x00450970 )
