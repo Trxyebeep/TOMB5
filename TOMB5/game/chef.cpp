7e788dd381a30ae02aaab6d860e5b8433e5522d2
Troye <woopstombraider@gmail.com>
2023-02-22 01:01:43 -0600

    style chef and crow

--- TOMB5/game/chef.cpp
+++ TOMB5/game/chef.cpp
@@ -9,157 +9,150 @@
 #include "control.h"
 #include "lara.h"
 
-BITE_INFO knife = {0, 200, 0, 13};
+static BITE_INFO knife = { 0, 200, 0, 13 };
 
 void InitialiseChef(short item_number)
 {
-	ITEM_INFO* chef;
+	ITEM_INFO* item;
 
-	chef = &items[item_number];
+	item = &items[item_number];
 	InitialiseCreature(item_number);
-	chef->anim_number = objects[CHEF].anim_index;
-	chef->frame_number = anims[objects[CHEF].anim_index].frame_base;
-	chef->current_anim_state = 1;
-	chef->goal_anim_state = 1;
-	chef->pos.x_pos += (192 * phd_sin(chef->pos.y_rot)) >> 14;
-	chef->pos.z_pos += (192 * phd_cos(chef->pos.y_rot)) >> 14;
+	item->anim_number = objects[CHEF].anim_index;
+	item->frame_number = anims[objects[CHEF].anim_index].frame_base;
+	item->current_anim_state = 1;
+	item->goal_anim_state = 1;
+	item->pos.x_pos += 192 * phd_sin(item->pos.y_rot) >> 14;
+	item->pos.z_pos += 192 * phd_cos(item->pos.y_rot) >> 14;
 }
 
 void ChefControl(short item_number)
 {
-	ITEM_INFO* chef;
-	CREATURE_INFO* creature;
-	AI_INFO ai;
-	AI_INFO laraAI;
+	ITEM_INFO* item;
+	CREATURE_INFO* chef;
+	AI_INFO info;
+	AI_INFO larainfo;
 	long dx, dz;
 	short angle, head, torso_y, torso_x;
 
 	if (!CreatureActive(item_number))
 		return;
 
-	chef = &items[item_number];
-	creature = (CREATURE_INFO*)chef->data;
+	item = &items[item_number];
+	chef = (CREATURE_INFO*)item->data;
 	head = 0;
 	torso_y = 0;
 	torso_x = 0;
 
-	if (chef->ai_bits)
-		GetAITarget(creature);
-	else if (creature->hurt_by_lara)
-		creature->enemy = lara_item;
+	if (item->ai_bits)
+		GetAITarget(chef);
+	else if (chef->hurt_by_lara)
+		chef->enemy = lara_item;
 
-	CreatureAIInfo(chef, &ai);
+	CreatureAIInfo(item, &info);
 
-	if (creature->enemy == lara_item)
+	if (chef->enemy == lara_item)
 	{
-		laraAI.angle = ai.angle;
-		laraAI.distance = ai.distance;
+		larainfo.angle = info.angle;
+		larainfo.distance = info.distance;
 	}
 	else
 	{
-		dx = lara_item->pos.x_pos - chef->pos.x_pos;
-		dz = lara_item->pos.z_pos - chef->pos.z_pos;
-		laraAI.angle = (short)(phd_atan(dz, dx) - chef->pos.y_rot);
-		laraAI.ahead = 1;
-
-		if (laraAI.angle <= -16384 || laraAI.angle >= 16384)
-			laraAI.ahead = 0;
-
-		laraAI.distance = SQUARE(dz) + SQUARE(dx);
+		dx = lara_item->pos.x_pos - item->pos.x_pos;
+		dz = lara_item->pos.z_pos - item->pos.z_pos;
+		larainfo.angle = short(phd_atan(dz, dx) - item->pos.y_rot);
+		larainfo.ahead = larainfo.angle > -0x4000 && larainfo.angle < 0x4000;
+		larainfo.distance = SQUARE(dz) + SQUARE(dx);
 	}
 
-	GetCreatureMood(chef, &ai, 1);
-	CreatureMood(chef, &ai, 1);
+	GetCreatureMood(item, &info, 1);
+	CreatureMood(item, &info, 1);
+	angle = CreatureTurn(item, chef->maximum_turn);
 
-	angle = CreatureTurn(chef, creature->maximum_turn);
-
-	if (ai.ahead)
+	if (info.ahead)
 	{
-		torso_y = ai.angle >> 1;
-		head = ai.angle >> 1;
-		torso_x = ai.x_angle;
+		torso_y = info.angle >> 1;
+		head = info.angle >> 1;
+		torso_x = info.x_angle;
 	}
 
-	creature->maximum_turn = 0;
+	chef->maximum_turn = 0;
 
-	switch (chef->current_anim_state)
+	switch (item->current_anim_state)
 	{
 	case 1:
-		if (abs(lara_item->pos.y_pos - chef->pos.y_pos) < 1024 && ai.distance < 2359296 &&
-			(chef->touch_bits || lara_item->speed > 15 || chef->hit_status || TargetVisible(chef, &laraAI)))
+
+		if (abs(lara_item->pos.y_pos - item->pos.y_pos) < 1024 && info.distance < 0x240000 &&
+			(item->touch_bits || lara_item->speed > 15 || item->hit_status || TargetVisible(item, &larainfo)))
 		{
-			chef->goal_anim_state = 2;
-			creature->alerted = 1;
-			chef->ai_bits = 0;
+			item->goal_anim_state = 2;
+			chef->alerted = 1;
+			item->ai_bits = 0;
 		}
 
 		break;
 
 	case 2:
-		creature->maximum_turn = 0;
+		chef->maximum_turn = 0;
 
-		if (ai.angle >= 0)
-			chef->pos.y_rot -= 364;
+		if (info.angle < 0)
+			item->pos.y_rot += 364;
 		else
-			chef->pos.y_rot += 364;
+			item->pos.y_rot -= 364;
 
-		if (chef->frame_number == anims[chef->anim_number].frame_end)
-			chef->pos.y_rot += 32768;
+		if (item->frame_number == anims[item->anim_number].frame_end)
+			item->pos.y_rot += 0x8000;
 
 		break;
 
 	case 3:
-		creature->maximum_turn = 0;
+		chef->maximum_turn = 0;
 
-		if (abs(ai.angle) >= 364)
-		{
-			if (ai.angle >= 0)
-				chef->pos.y_rot += 364;
-			else
-				chef->pos.y_rot -= 364;
-		}
+		if (abs(info.angle) < 364)
+			item->pos.y_rot += info.angle;
+		else if (info.angle < 0)
+			item->pos.y_rot -= 364;
 		else
-			chef->pos.y_rot += ai.angle;
+			item->pos.y_rot += 364;
 
-		if (!creature->flags && chef->touch_bits & 0x2000 && chef->frame_number > anims[chef->anim_number].frame_base + 10)
+		if (!chef->flags && item->touch_bits & 0x2000 && item->frame_number > anims[item->anim_number].frame_base + 10)
 		{
 			lara_item->hit_points -= 80;
 			lara_item->hit_status = 1;
-			CreatureEffectT(chef, &knife, 20, chef->pos.y_rot, DoBloodSplat);
-			SoundEffect(70, &chef->pos, 0);
-			creature->flags = 1;
+			CreatureEffectT(item, &knife, 20, item->pos.y_rot, DoBloodSplat);
+			SoundEffect(70, &item->pos, 0);
+			chef->flags = 1;
 		}
 
 		break;
 
 	case 4:
-		creature->flags = 0;
-		creature->maximum_turn = 364;
+		chef->flags = 0;
+		chef->maximum_turn = 364;
 
-		if (ai.distance >= 465124)
+		if (info.distance < 0x718E4)
 		{
-			if (ai.angle > 20480 || ai.angle < -20480)
-				chef->goal_anim_state = 2;
-			else if (creature->mood == ATTACK_MOOD)
-				chef->goal_anim_state = 5;
+			if (info.bite)
+				item->goal_anim_state = 3;
 		}
-		else if (ai.bite)
-			chef->goal_anim_state = 3;
+		else if (info.angle > 0x5000 || info.angle < -0x5000)
+			item->goal_anim_state = 2;
+		else if (chef->mood == ATTACK_MOOD)
+			item->goal_anim_state = 5;
 
 		break;
 
 	case 5:
-		creature->maximum_turn = 1274;
+		chef->maximum_turn = 1274;
 
-		if (ai.distance < 465124 || ai.angle > 20480 || ai.angle < -20480 || creature->mood != ATTACK_MOOD)
-			chef->goal_anim_state = 4;
+		if (info.distance < 0x718E4 || info.angle > 0x5000 || info.angle < -0x5000 || chef->mood != ATTACK_MOOD)
+			item->goal_anim_state = 4;
 
 		break;
 	}
 
-	CreatureTilt(chef, 0);
-	CreatureJoint(chef, 0, torso_y);
-	CreatureJoint(chef, 1, torso_x);
-	CreatureJoint(chef, 2, head);
+	CreatureTilt(item, 0);
+	CreatureJoint(item, 0, torso_y);
+	CreatureJoint(item, 1, torso_x);
+	CreatureJoint(item, 2, head);
 	CreatureAnimation(item_number, angle, 0);
 }
