c35e79d576aa555a1a37f4d1775eef25067c94da
Troye <woopstombraider@gmail.com>
2023-02-21 22:38:51 -0600

    style andrea and andy

--- TOMB5/game/andrea.cpp
+++ TOMB5/game/andrea.cpp
@@ -37,13 +37,10 @@ void ControlPropeller(short item_number)
 	{
 		TestTriggersAtXYZ(item->pos.x_pos, item->pos.y_pos, item->pos.z_pos, item->room_number, 1, 0);
 
-		if (item->current_anim_state == 1)
-		{
-			if (item->frame_number == anims[item->anim_number].frame_end)
-				return;
-		}
-		else
+		if (item->current_anim_state != 1)
 			item->goal_anim_state = 1;
+		else if (item->frame_number == anims[item->anim_number].frame_end)
+			return;
 	}
 
 	if (item->current_anim_state == 1)
@@ -55,6 +52,7 @@ void ControlPropeller(short item_number)
 	b[2] = item->pos.y_pos + bptr[2];
 	b[3] = item->pos.y_pos + bptr[3];
 
+	//Vertical?
 	if (item->object_number == PROPELLER_V)
 	{
 		phd_PushUnitMatrix();
@@ -90,101 +88,102 @@ void ControlPropeller(short item_number)
 				dy = frame * dy / 120;
 
 			lara_item->pos.y_pos += dy;
-			return;
 		}
+
+		return;
 	}
-	else
+
+	//Horizontal
+	switch (item->pos.y_rot)
 	{
-		switch (item->pos.y_rot)
+	case 0x4000:
+		b[0] = item->pos.x_pos + bptr[4];
+		b[1] = item->pos.x_pos + bptr[5];
+		b[4] = item->pos.z_pos - bptr[1];
+		b[5] = item->pos.z_pos - bptr[0];
+		Xaxis = 0;
+		Zaxis = 1;
+		break;
+
+	case -0x4000:
+		b[0] = item->pos.x_pos - bptr[5];
+		b[1] = item->pos.x_pos - bptr[4];
+		b[4] = item->pos.z_pos + bptr[0];
+		b[5] = item->pos.z_pos + bptr[1];
+		Xaxis = 0;
+		Zaxis = 1;
+		break;
+
+	case -0x8000:
+		b[0] = item->pos.x_pos - bptr[1];
+		b[1] = item->pos.x_pos - bptr[0];
+		b[4] = item->pos.z_pos - bptr[5];
+		b[5] = item->pos.z_pos - bptr[4];
+		Xaxis = 1;
+		Zaxis = 0;
+		break;
+
+	default:
+		b[0] = item->pos.x_pos + *bptr;
+		b[1] = item->pos.x_pos + bptr[1];
+		b[4] = item->pos.z_pos + bptr[4];
+		b[5] = item->pos.z_pos + bptr[5];
+		Xaxis = 1;
+		Zaxis = 0;
+		break;
+	}
+
+	TriggerFanEffects(b, 2, item->pos.y_rot, frame);
+	TriggerFanEffects(b, -2, item->pos.y_rot, frame);
+
+	if (lara_item->pos.y_pos < b[2] || lara_item->pos.y_pos > b[3])
+		return;
+
+	if (Zaxis)
+	{
+		if (lara_item->pos.x_pos >= b[0] && lara_item->pos.x_pos <= b[1])
 		{
-		case 16384:
-			b[0] = item->pos.x_pos + bptr[4];
-			b[1] = item->pos.x_pos + bptr[5];
-			b[4] = item->pos.z_pos - bptr[1];
-			b[5] = item->pos.z_pos - bptr[0];
-			Xaxis = 0;
-			Zaxis = 1;
-			break;
+			dz1 = abs(lara_item->pos.z_pos - b[4]);
+			dz2 = abs(lara_item->pos.z_pos - b[5]);
 
-		case -16384:
-			b[0] = item->pos.x_pos - bptr[5];
-			b[1] = item->pos.x_pos - bptr[4];
-			b[4] = item->pos.z_pos + bptr[0];
-			b[5] = item->pos.z_pos + bptr[1];
-			Xaxis = 0;
-			Zaxis = 1;
-			break;
+			if (dz2 >= dz1)
+				Zaxis = -Zaxis;
+			else
+				dz1 = dz2;
 
-		case -32768:
-			b[0] = item->pos.x_pos - bptr[1];
-			b[1] = item->pos.x_pos - bptr[0];
-			b[4] = item->pos.z_pos - bptr[5];
-			b[5] = item->pos.z_pos - bptr[4];
-			Xaxis = 1;
-			Zaxis = 0;
-			break;
+			if (dz1 < item->item_flags[0])
+			{
+				dy = 96 * Zaxis * (item->item_flags[0] - dz1) / item->item_flags[0];
 
-		default:
-			b[0] = item->pos.x_pos + *bptr;
-			b[1] = item->pos.x_pos + bptr[1];
-			b[4] = item->pos.z_pos + bptr[4];
-			b[5] = item->pos.z_pos + bptr[5];
-			Xaxis = 1;
-			Zaxis = 0;
-			break;
+				if (item->current_anim_state == 1)
+					dy = frame * dy / 120;
+
+				lara_item->pos.z_pos += dy;
+			}
 		}
 
-		TriggerFanEffects(b, 2, item->pos.y_rot, frame);
-		TriggerFanEffects(b, -2, item->pos.y_rot, frame);
+		return;
+	}
+	
+	//Xaxis
+	if (lara_item->pos.z_pos >= b[4] && lara_item->pos.z_pos <= b[5])
+	{
+		dx1 = abs(lara_item->pos.x_pos - b[0]);
+		dx2 = abs(lara_item->pos.x_pos - b[1]);
 
-		if (lara_item->pos.y_pos >= b[2] && lara_item->pos.y_pos <= b[3])
+		if (dx2 >= dx1)
+			Xaxis = -Xaxis;
+		else
+			dx1 = dx2;
+
+		if (dx1 < item->item_flags[0])
 		{
-			if (Zaxis)
-			{
-				if (lara_item->pos.x_pos >= b[0] && lara_item->pos.x_pos <= b[1])
-				{
-					dz1 = abs(lara_item->pos.z_pos - b[4]);
-					dz2 = abs(lara_item->pos.z_pos - b[5]);
+			dy = 96 * Xaxis * (item->item_flags[0] - dx1) / item->item_flags[0];
 
-					if (dz2 >= dz1)
-						Zaxis = -Zaxis;
-					else
-						dz1 = dz2;
+			if (item->current_anim_state == 1)
+				dy = frame * dy / 120;
 
-					if (dz1 < item->item_flags[0])
-					{
-						dy = 96 * Zaxis * (item->item_flags[0] - dz1) / item->item_flags[0];
-
-						if (item->current_anim_state == 1)
-							dy = frame * dy / 120;
-
-						lara_item->pos.z_pos += dy;
-					}
-				}
-			}
-			else
-			{
-				if (lara_item->pos.z_pos >= b[4] && lara_item->pos.z_pos <= b[5])
-				{
-					dx1 = abs(lara_item->pos.x_pos - b[0]);
-					dx2 = abs(lara_item->pos.x_pos - b[1]);
-
-					if (dx2 >= dx1)
-						Xaxis = -Xaxis;
-					else
-						dx1 = dx2;
-
-					if (dx1 < item->item_flags[0])
-					{
-						dy = 96 * Xaxis * (item->item_flags[0] - dx1) / item->item_flags[0];
-
-						if (item->current_anim_state == 1)
-							dy = frame * dy / 120;
-
-						lara_item->pos.x_pos += dy;
-					}
-				}
-			}
+			lara_item->pos.x_pos += dy;
 		}
 	}
 }
@@ -197,12 +196,7 @@ void TriggerFanEffects(long* b, long y, short angle, long rate)
 	if (abs(y) == 1)
 	{
 		dx = (b[0] + b[1]) >> 1;
-
-		if (y >= 0)
-			dy = b[3];
-		else
-			dy = b[2];
-
+		dy = y < 0 ? b[2] : b[3];
 		dz = (b[4] + b[5]) >> 1;
 	}
 	else
@@ -211,103 +205,76 @@ void TriggerFanEffects(long* b, long y, short angle, long rate)
 
 		if (angle & 0x7FFF)
 		{
-
-			if (y >= 0)
-				dz = b[5];
-			else
-				dz = b[4];
-
 			dx = (b[0] + b[1]) >> 1;
+			dz = y < 0 ? b[4] : b[5];
 		}
 		else
 		{
-			if (y >= 0)
-				dx = b[1];
-			else
-				dx = b[0];
-
+			dx = y < 0 ? b[0] : b[1];
 			dz = (b[4] + b[5]) >> 1;
 		}
 	}
 
-	if (abs(camera.pos.x - dx) <= 7168 && abs(camera.pos.y - dy) <= 7168 && abs(camera.pos.z - dz) <= 7168)
+	if (abs(camera.pos.x - dx) > 7168 || abs(camera.pos.y - dy) > 7168 || abs(camera.pos.z - dz) > 7168)
+		return;
+
+	sptr = &spark[GetFreeSpark()];
+	sptr->On = 1;
+	sptr->sR = 0;
+	sptr->sG = 0;
+	sptr->sB = 0;
+	sptr->dR = uchar((48 * rate) >> 7);
+	sptr->dG = sptr->dR;
+	sptr->dB = uchar((rate * ((GetRandomControl() & 8) + 48)) >> 7);
+	sptr->ColFadeSpeed = 4;
+	sptr->FadeToBlack = 8;
+	sptr->TransType = 2;
+	sptr->Life = (GetRandomControl() & 3) + 20;
+	sptr->sLife = sptr->Life;
+
+	if (abs(y) == 1)
 	{
-		sptr = &spark[GetFreeSpark()];
-		sptr->On = 1;
-		sptr->sR = 0;
-		sptr->sG = 0;
-		sptr->sB = 0;
-		sptr->dR = (uchar)((48 * rate) >> 7);
-		sptr->dG = (uchar)((48 * rate) >> 7);
-		sptr->dB = (uchar)((rate * ((GetRandomControl() & 8) + 48)) >> 7);
-		sptr->ColFadeSpeed = 4;
-		sptr->FadeToBlack = 8;
-		sptr->TransType = 2;
-		sptr->Life = (GetRandomControl() & 3) + 20;
-		sptr->sLife = sptr->Life;
+		dx = GetRandomControl() % ((3 * (b[1] - b[0])) >> 3);
+		dz = GetRandomControl() << 1;
+		sptr->x = ((b[0] + b[1]) >> 1) + (dx * phd_sin(dz) >> 14);
+		sptr->y = y < 0 ? b[2] : b[3];
+		sptr->z = ((b[4] + b[5]) >> 1) + (dx * phd_cos(dz) >> 14);
+		sptr->Xvel = 0;
+		sptr->Yvel = short(32 * y * ((GetRandomControl() & 0x1F) + 224));
+		sptr->Zvel = 0;
+	}
+	else
+	{
+		dx = GetRandomControl() % ((3 * (b[3] - b[2])) >> 3);
+		dz = GetRandomControl() << 1;
 
-		if (abs(y) == 1)
+		if (angle & 0x7FFF)
 		{
-			dx = (3 * (b[1] - b[0])) >> 3;
-			dx = GetRandomControl() % dx;
-			dz = GetRandomControl() << 1;
-			sptr->x = ((b[0] + b[1]) >> 1) + ((phd_sin(dz) * dx) >> 14);
-			sptr->z = ((phd_cos(dz) * dx) >> 14) + ((b[4] + b[5]) >> 1);
-
-			if (y >= 0)
-				sptr->y = b[3];
-			else
-				sptr->y = b[2];
-
-			sptr->Zvel = 0;
+			sptr->x = ((b[0] + b[1]) >> 1) + (dx * phd_cos(dz) >> 14);
+			sptr->y = ((b[3] + b[2]) >> 1) + (dx * phd_sin(dz) >> 14);
+			sptr->z = y < 0 ? b[4] : b[5];
 			sptr->Xvel = 0;
-			sptr->Yvel = (short)(32 * y * ((GetRandomControl() & 0x1F) + 224));
+			sptr->Zvel = short(y * ((GetRandomControl() & 0x1F) + 224) << 4);
 		}
 		else
 		{
-			dx = (3 * (b[3] - b[2])) >> 3;
-			dx = GetRandomControl() % dx;
-			dz = GetRandomControl() << 1;
-			sptr->y = (b[3] + b[2]) >> 1;
-
-			if (angle & 0x7FFF)
-			{
-				if (y >= 0)
-					sptr->z = b[5];
-				else
-					sptr->z = b[4];
-
-				sptr->x = (b[0] + b[1]) >> 1;
-				sptr->y += (dx * phd_sin(dz)) >> 14;
-				sptr->x += (dx * phd_cos(dz)) >> 14;
-				sptr->Xvel = 0;
-				sptr->Zvel = (short)(y * ((GetRandomControl() & 0x1F) + 224) << 4);
-			}
-			else
-			{
-				if (y >= 0)
-					sptr->x = b[1];
-				else
-					sptr->x = b[0];
-
-				sptr->z = (b[4] + b[5]) >> 1;
-				sptr->y += (dx * phd_sin(dz)) >> 14;
-				sptr->z += (dx * phd_cos(dz)) >> 14;
-				sptr->Zvel = 0;
-				sptr->Xvel = (short)(y * ((GetRandomControl() & 0x1F) + 224) << 4);
-			}
-
-			sptr->Yvel = 0;
+			sptr->x = y < 0 ? b[0] : b[1];
+			sptr->y = ((b[3] + b[2]) >> 1) + (dx * phd_sin(dz) >> 14);
+			sptr->z = ((b[4] + b[5]) >> 1) + (dx * phd_cos(dz) >> 14);
+			sptr->Xvel = short(y * ((GetRandomControl() & 0x1F) + 224) << 4);
+			sptr->Zvel = 0;
 		}
 
-		sptr->Friction = 85;
-		sptr->MaxYvel = 0;
-		sptr->Xvel = (short)((rate * sptr->Xvel) >> 7);
-		sptr->Yvel = (short)((rate * sptr->Yvel) >> 7);
-		sptr->Zvel = (short)((rate * sptr->Zvel) >> 7);
-		sptr->Gravity = 0;
-		sptr->Flags = 0;
+		sptr->Yvel = 0;
 	}
+
+	sptr->Friction = 85;
+	sptr->MaxYvel = 0;
+	sptr->Xvel = short(rate * sptr->Xvel >> 7);
+	sptr->Yvel = short(rate * sptr->Yvel >> 7);
+	sptr->Zvel = short(rate * sptr->Zvel >> 7);
+	sptr->Gravity = 0;
+	sptr->Flags = 0;
 }
 
 void ControlRaisingCog(short item_number)
@@ -323,65 +290,63 @@ void ControlRaisingCog(short item_number)
 		return;
 
 	if (item->item_flags[0] >= 3)
-		AnimateItem(item);
-	else
 	{
-		if (item->item_flags[2] >= 256)
+		AnimateItem(item);
+		return;
+	}
+
+	if (item->item_flags[2] >= 256)
+	{
+		item->item_flags[2] = 0;
+		item->item_flags[0]++;
+
+		if (item->item_flags[0] == 3)
 		{
-			item->item_flags[2] = 0;
-			item->item_flags[0]++;
-
-			if (item->item_flags[0] == 3)
+			for (NumTrigs = (short)GetSwitchTrigger(item, TriggerItems, 1); NumTrigs; NumTrigs--)
 			{
-				for (NumTrigs = (short)GetSwitchTrigger(item, TriggerItems, 1); NumTrigs; NumTrigs--)
-				{
-					triggered = &items[TriggerItems[NumTrigs - 1]];
+				triggered = &items[TriggerItems[NumTrigs - 1]];
 
-					if (triggered->object_number == PULLEY)
+				if (triggered->object_number == PULLEY)
+				{
+					if (triggered->room_number == item->item_flags[1])
 					{
-						if (triggered->room_number == item->item_flags[1])
-						{
-							triggered->item_flags[1] = 0;
-							triggered->flags |= IFL_TRIGGERED;
-						}
-						else
-							triggered->item_flags[1] = 1;
+						triggered->item_flags[1] = 0;
+						triggered->flags |= IFL_TRIGGERED;
 					}
-					else if (triggered->object_number == TRIGGER_TRIGGERER)
-					{
-						AddActiveItem(TriggerItems[NumTrigs - 1]);
-						triggered->status = ITEM_ACTIVE;
-						triggered->flags |= IFL_CODEBITS;
-					}
+					else
+						triggered->item_flags[1] = 1;
+				}
+				else if (triggered->object_number == TRIGGER_TRIGGERER)
+				{
+					AddActiveItem(TriggerItems[NumTrigs - 1]);
+					triggered->status = ITEM_ACTIVE;
+					triggered->flags |= IFL_CODEBITS;
 				}
 			}
-
-			RemoveActiveItem(item_number);
-			item->status = ITEM_INACTIVE;
-			item->flags &= ~IFL_CODEBITS;
 		}
-		else
+
+		RemoveActiveItem(item_number);
+		item->status = ITEM_INACTIVE;
+		item->flags &= ~IFL_CODEBITS;
+	}
+	else
+	{
+		if (!item->item_flags[2])
 		{
-			if (!item->item_flags[2])
-			{
-				InitialiseSpotCam(9);
-				bUseSpotCam = 1;
-			}
-
-			if (item->item_flags[2] >= 31)
-			{
-				if (item->item_flags[2] <= 224)
-					vol = 31;
-				else
-					vol = 255 - item->item_flags[2];
-			}
-			else
-				vol = item->item_flags[2];
-
-			SoundEffect(SFX_BLK_PLAT_RAISE_LOW, &item->pos, (vol << 8) | SFX_SETVOL);
-			item->item_flags[2] += 2;
-			item->pos.y_pos -= 2;
+			InitialiseSpotCam(9);
+			bUseSpotCam = 1;
 		}
+
+		if (item->item_flags[2] < 31)
+			vol = item->item_flags[2];
+		else if (item->item_flags[2] > 224)
+			vol = 255 - item->item_flags[2];
+		else
+			vol = 31;
+
+		SoundEffect(SFX_BLK_PLAT_RAISE_LOW, &item->pos, (vol << 8) | SFX_SETVOL);
+		item->item_flags[2] += 2;
+		item->pos.y_pos -= 2;
 	}
 }
 
@@ -402,38 +367,31 @@ void ControlPortalDoor(short item_number)
 		SoundEffect(SFX_EXPLOSION1, &item->pos, SFX_SETPITCH | 0x400000);
 		item->item_flags[0]++;
 	}
-	else
+	else if (item->item_flags[0] < 12)
 	{
-		if (item->item_flags[0] < 12)
-		{
-			if (FlashFader > 2)
-			{
-				FlashFader >>= 1;
-				FlashFader &= ~1;
-			}
+		if (FlashFader > 2)
+			FlashFader = (FlashFader >> 1) & ~1;
 
-			if (GlobalCounter & 1)
-			{
-				FlashFadeR = 192;
-				FlashFadeG = (GetRandomControl() & 0x1F) + 160;
-				FlashFadeB = GetRandomControl() & 0x7F;
-				FlashFader = 32;
-				item->item_flags[0]++;
-			}
-		}
-		else if (item->item_flags[0] == 12)
+		if (GlobalCounter & 1)
 		{
-			SoundEffect(SFX_GOD_HEAD_BLAST, &item->pos, SFX_SETPITCH | 0x400000);
 			FlashFadeR = 192;
 			FlashFadeG = (GetRandomControl() & 0x1F) + 160;
-			FlashFadeB = 0;
+			FlashFadeB = GetRandomControl() & 0x7F;
 			FlashFader = 32;
 			item->item_flags[0]++;
 		}
 	}
+	else if (item->item_flags[0] == 12)
+	{
+		SoundEffect(SFX_GOD_HEAD_BLAST, &item->pos, SFX_SETPITCH | 0x400000);
+		FlashFadeR = 192;
+		FlashFadeG = (GetRandomControl() & 0x1F) + 160;
+		FlashFadeB = 0;
+		FlashFader = 32;
+		item->item_flags[0]++;
+	}
 
 	sptr = &spark[GetFreeSpark()];
-	x = (GetRandomControl() & 0x3F) - 32;
 	sptr->On = 1;
 	sptr->sR = 0;
 	sptr->sG = 0;
@@ -446,19 +404,20 @@ void ControlPortalDoor(short item_number)
 	sptr->TransType = 2;
 	sptr->Life = (GetRandomControl() & 3) + 16;
 	sptr->sLife = sptr->Life;
-	sptr->x = x + item->pos.x_pos;
-	sptr->y = -(GetRandomControl() & 0x3FF) + item->pos.y_pos;
-	sptr->z = ((GetRandomControl() & 0x3FF) - 512) + item->pos.z_pos;
-	sptr->Xvel = (short)(x << 3);
+	x = (GetRandomControl() & 0x3F) - 32;
+	sptr->x = item->pos.x_pos + x;
+	sptr->y = item->pos.y_pos - (GetRandomControl() & 0x3FF);
+	sptr->z = item->pos.z_pos + ((GetRandomControl() & 0x3FF) - 512);
+	sptr->Xvel = short(x << 3);
 	sptr->Yvel = 0;
 	sptr->Zvel = 0;
 	sptr->Friction = 6;
 	sptr->Gravity = 0;
 	sptr->MaxYvel = 0;
 	sptr->Flags = 10;
-	sptr->dSize = (GetRandomControl() & 3) + 8;
-	sptr->sSize = sptr->dSize;
-	sptr->Size = sptr->dSize;
+	sptr->Size = (GetRandomControl() & 3) + 8;
+	sptr->sSize = sptr->Size;
+	sptr->dSize = sptr->Size;
 	sptr->Scalar = 1;
 	sptr->Def = objects[DEFAULT_SPRITES].mesh_index + 14;
 }
@@ -530,12 +489,10 @@ void ControlRaisingPlinth(short item_number)
 	{
 		if (item->item_flags[0] == 4)
 		{
-			// Timer running
 			item->item_flags[1]--;
 
 			if (!item->item_flags[1])
 			{
-				// Timer done
 				item2 = &items[item->item_flags[3] & 0xFF];
 				item2->flags = (item2->flags & ~IFL_CODEBITS) | IFL_TRIGGERED;
 				item->item_flags[0] = 6;
@@ -545,7 +502,6 @@ void ControlRaisingPlinth(short item_number)
 		}
 		else if (item->item_flags[0] == 6)
 		{
-			// Lower the plinth
 			item->item_flags[1] -= 8;
 			vol = item->item_flags[1];
 
@@ -569,7 +525,6 @@ void ControlRaisingPlinth(short item_number)
 			}
 			else if (item->item_flags[1] < -60)
 			{
-				// Plinth lowering done
 				item2 = &items[item->item_flags[2] & 0xFF];
 				item2->item_flags[1] = 0;
 				item2->flags |= IFL_TRIGGERED;
@@ -583,7 +538,6 @@ void ControlRaisingPlinth(short item_number)
 	}
 	else if (item->item_flags[0] < 3)
 	{
-		// Raise the plinth, chain pulled
 		vol = item->item_flags[1];
 
 		if (vol < 256)
@@ -610,7 +564,6 @@ void ControlRaisingPlinth(short item_number)
 
 			if (item->item_flags[0] == 3)
 			{
-				// Third raise of the plinth, must enable timer
 				item->item_flags[1] = item->trigger_flags * 0x1E;
 				item->item_flags[0] = 4;
 				item_number2 = item->item_flags[3] & 0xFF;
@@ -625,7 +578,6 @@ void ControlRaisingPlinth(short item_number)
 			}
 			else
 			{
-				// First two raises of the plinth
 				RemoveActiveItem(item_number);
 				item->flags &= ~IFL_CODEBITS;
 				item->status = 0;
@@ -634,13 +586,11 @@ void ControlRaisingPlinth(short item_number)
 	}
 	else if (item->item_flags[0] == 4)
 	{
-		// Touched trigger to disable timer, plinth won't lower
 		item->item_flags[0] = 5;
 		item->item_flags[1] = 0;
 	}
 	else if (item->item_flags[0] == 5 && !item->item_flags[1] && (items[item->item_flags[3] >> 8].flags & IFL_CLEARBODY) != 0)
 	{
-		// Grabbed item on top of plinth. Plinth disabled.
 		FlipMap(3);
 		flipmap[3] ^= IFL_CODEBITS;
 		item->item_flags[1] = 1;
