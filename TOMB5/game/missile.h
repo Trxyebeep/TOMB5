4964a30e13e53a4351edaa3de290b8ba29d193e9
Troye <woopstombraider@gmail.com>
2023-05-04 06:08:50 -0500

    style missile/moveblok;

--- TOMB5/game/missile.h
+++ TOMB5/game/missile.h
@@ -2,6 +2,5 @@
 #include "../global/types.h"
 
 void ControlBodyPart(short fx_number);
-void ShootAtLara(FX_INFO* fx);
 void ControlMissile(short fx_number);
 long ExplodeFX(FX_INFO* fx, long NoXZVel, short Num);
--- TOMB5/game/moveblok.h
+++ TOMB5/game/moveblok.h
@@ -3,7 +3,6 @@
 
 void InitialiseMovingBlock(short item_number);
 void MovableBlock(short item_number);
-void MovableBlockCollision(short item_number, ITEM_INFO* laraitem, COLL_INFO* coll);
+void MovableBlockCollision(short item_number, ITEM_INFO* l, COLL_INFO* coll);
 long TestBlockPush(ITEM_INFO* item, long height, ushort quadrant);
 long TestBlockPull(ITEM_INFO* item, long height, ushort quadrant);
-void ClearMovableBlockSplitters(long x, long y, long z, short room_number);
